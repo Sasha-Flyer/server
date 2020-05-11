50a28ebc9df6ce026452b41662a11bf8d1dc091e
Sasha-Flyer <nonameznet97@gmail.com>
2020-05-12 01:23:55 +0300

    add client count

--- server_status.py
+++ server_status.py
@@ -1,7 +1,9 @@
 import psutil
 import system_pb2
 from aiohttp import web, ClientTimeout
-from asyncio import sleep
+from asyncio import sleep, ensure_future
+
+client_count = 0
 
 
 def get_sys_info():
@@ -14,17 +16,27 @@ def get_sys_info():
     system.ssd_usage = psutil.disk_usage('/').percent
     return system.SerializeToString()
 
+async def reader(ws):
+    async for msg in ws: pass
 
 async def websocket_handler(request):
+    global client_count
+    client_count += 1
+    print("клиент подключился, всего клиентов: {0}".format(client_count))
     ws = web.WebSocketResponse(timeout=ClientTimeout(sock_read=10))
     await ws.prepare(request)
-    while True:
-        info = get_sys_info()
-        await ws.send_bytes(info)
-        await sleep(1)
-        if ws.closed:
-            break
-    return ws
+    ensure_future(reader(ws))
+    try:
+        while True:
+            info = get_sys_info()
+            await ws.send_bytes(info)
+            await sleep(1)
+            if ws.closed:
+                break
+        return ws
+    finally:
+        client_count -= 1
+        print("клиент отключился, всего клиентов: {0}".format(client_count))
 
 app = web.Application()
 app.add_routes([web.get('/', websocket_handler)])
